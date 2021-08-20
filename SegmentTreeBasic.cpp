482812de378588a0461ba57f7144b027dcb1a4c4
Dipjoy Basak <62418148+DIPJOY10@users.noreply.github.com>
2021-08-20 12:51:01 +0530

    Update SegmentTreeBasic.cpp

--- SegmentTreeBasic.cpp
+++ SegmentTreeBasic.cpp
@@ -1,6 +1,69 @@
 //Range Query DS:SegTree Basic Implementation
-//o(logn) for updates and queries and o(n) for build
+//o(logn) for point updates and range queries and o(n) for build
 
+
+//Recursive Build Version
+struct Segtree
+{
+    int n;
+    vector<int> tree;
+
+    Segtree(int n1, vector<int> v)
+    {
+        n = n1;
+        tree.resize(2 * n);
+        build(1, 0, n - 1, v);
+    }
+
+    void build(int node, int left, int right, vector<int> v)
+    {
+        if (left == right)
+        {
+            tree[node] = v[left];
+            return;
+        }
+        int mid = (left + right) / 2;
+        build(node << 1, left, mid, v);
+        build(node << 1 | 1, mid + 1, right, v);
+        tree[node] = tree[node << 1] + tree[node << 1 | 1];
+    }
+
+    void update(int node, int left, int right, int idx, int val)
+    {
+        if (left == right)
+        {
+            tree[node] = val;
+            return;
+        }
+
+        int mid = (left + right) / 2;
+        if (mid >= idx)
+            update(node << 1, left, mid, idx, val);
+        else
+            update(node << 1 | 1, mid + 1, right, idx, val);
+        tree[node] = tree[node << 1] + tree[node << 1 | 1];
+    }
+
+    int query(int node, int left, int right, int lquery, int rquery)
+    {
+        if (lquery <= left && rquery >= right)
+        { //full overlap
+            return tree[node];
+        }
+
+        if (lquery > right || rquery < left)
+        {             //no overlap
+            return 0; //identity element
+        }
+        //partial overlap
+        int mid = (left + right) / 2;
+        return query(node << 1, left, mid, lquery, rquery) +
+               query(node << 1 | 1, mid + 1, right, lquery, rquery);
+    }
+};
+
+
+//Iterative Build version
 struct Segtree
 {
     int n;
@@ -53,3 +116,4 @@ struct Segtree
                query(2 * node + 1, mid + 1, end, l, r);//can change as per operation
     }
 };
+

3972cfe20156ebcb9a82a1c88c8d361e7d9f67c8
Dipjoy Basak <62418148+DIPJOY10@users.noreply.github.com>
2022-01-27 10:29:52 +0530

    Update Bit Trie.cpp

--- Bit Trie.cpp
+++ Bit Trie.cpp
@@ -1,72 +1,42 @@
 //reference problem:https://codeforces.com/contest/1625/problem/D
 
-struct node
-{
-    node *child[2];
-    int cnt;
-    node()
-    {
-        child[0] = NULL;
-        child[1] = NULL;
-        cnt = 0;
-    }
-};
-
-struct TRIE
-{
-    node *root;
-    TRIE()
-    {
-        root = new node();
-    }
-
-    void insert(int num)
-    {
-        node *temp = root;
-        for (int bit = 30; bit >= 0; bit--)
-        {
-            int curr = ((1 << bit) & num) > 0;
-            if (!temp->child[curr])
-            {
-                temp->child[curr] = new node();
-            }
-            temp = temp->child[curr];
-            temp->cnt++;
+struct Node{
+        int ptr;
+        Node* child[2];
+        Node(){
+            ptr=0;
+            child[0]=NULL;
+            child[1]=NULL;
         }
-    }
-
-    int max_xor(int num)
-    {
-        node *temp = root;
-        int ans = 0;
-        for (int bit = 30; bit >= 0; bit--)
-        {
-            int curr = ((1 << bit) & num) > 0;
-            if (curr == 1)
-            {
-                if (temp->child[0])
-                {
-                    ans += (1 << bit);
-                    temp = temp->child[0];
+    };
+    struct TRIE{
+        Node* root;
+        TRIE(){
+            root=new Node();
+        }
+        void insert(int num){
+            Node* temp=root;
+            for(int i=30;i>=0;i--){
+                int set=((num>>i)&1);
+                if(!temp->child[set]){
+                    temp->child[set]=new Node();
                 }
-                else
-                {
-                    temp = temp->child[1];
-                }
-            }
-            else
-            {
-                if (temp->child[1])
-                {
-                    ans += (1 << bit);
-                    temp = temp->child[1];
-                }
-                else
-                {
-                    temp = temp->child[0];
-                }
+                temp=temp->child[set];
+                temp->ptr++;
             }
         }
-        return ans;
-    }
-};
+        int max_xor(int num){
+            int ans=0;
+            Node* temp=root;
+            for(int i=30;i>=0;i--){
+                int set=((num>>i)&1);
+                if(temp->child[!set]){
+                    ans+=(1<<i);
+                    temp=temp->child[!set];
+                }else{
+                    temp=temp->child[set];
+                }
+            }
+            return ans;
+        }
+    };
